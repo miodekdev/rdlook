e7ca1da95a93ee694e6d23741b3a6bba457155eb
Miod3k <maksymilianmiodonski@gmail.com>
2024-04-27 15:45:55 +0200

    Changed output style and code flow yet again

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ filesystem::path main_path;
 string phrase;
 size_t total_matches = 0;
 
-size_t findAll(const string& candidate) {
+size_t findAll(string& path, bool highlight = false) {
     size_t matches = 0;
     size_t position = 0;
-    while ((position = candidate.find(phrase, position)) != string::npos) {
+    while ((position = path.find(phrase, position)) != string::npos) {
         matches++;
         total_matches++;
         position += phrase.length();
@@ -20,28 +20,36 @@ size_t findAll(const string& candidate) {
     return matches;
 }
 
-void tryToFind(const filesystem::path& candidate) {
-    size_t name_matches = findAll(candidate.string());
-    size_t contents_matches = 0;
-    if (filesystem::is_regular_file(candidate)) {
-        ifstream file(candidate);
+void tryToFind(const filesystem::path& path, bool output = true) {
+    size_t matches = 0;
+    if (filesystem::is_regular_file(path)) {
+        ifstream file(path);
         string line;
         while (getline(file, line)) {
-            contents_matches += findAll(line);
+            matches += findAll(line);
         }
         file.close();
     }
-    if (name_matches != string::npos || contents_matches) {
-        cout << "(" << (contents_matches ? to_string(contents_matches) : "name") << ") ";
-        cout << filesystem::relative(candidate, main_path).string() << endl;
+
+    string filename = path.filename().string();
+    size_t highlight_position = filename.find(phrase);
+    string parent = filesystem::relative(path, main_path).parent_path().string();
+    if (highlight_position != string::npos || matches && output) {
+        if (matches) cout << "\033[1m(" << to_string(matches) << ")\033[0m ";
+        if (highlight_position != string::npos) {
+            filename.insert(highlight_position + phrase.length(), "\033[0m");
+            filename.insert(highlight_position, "\033[7m");
+        }
+        if (!parent.empty()) cout << parent << "\\";
+        cout << filename << endl;
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { // TODO better argument parsing, options
     cout << endl;
     switch (argc) {
         case 1:
-            cerr << "No directory or file specified";
+            cerr << "No key phrase specified.";
             return 1;
         case 2:
             main_path = filesystem::current_path();
@@ -51,19 +59,19 @@ int main(int argc, char *argv[]) {
             main_path = argv[1];
             phrase = argv[2];
     }
-    if (filesystem::is_regular_file(main_path)) {
-        tryToFind(main_path);
-    }
-    else if (filesystem::is_directory(main_path)) {
+    if (filesystem::is_directory(main_path)) {
         for (auto const& path : filesystem::recursive_directory_iterator(main_path)) {
             tryToFind(path);
         }
+        cout << endl;
+    }
+    else if (filesystem::is_regular_file(main_path)) {
+        tryToFind(main_path, false);
     }
     else {
-        cerr << "\"" << main_path << "\" is not a valid directory or a file";
+        cerr << "\"" << main_path << "\" is not a valid directory or a file.";
         return 1;
     }
-    cout << endl << "Found " << to_string(total_matches);
-    cout << " match" << ((total_matches != 1) ? "es" : "") << "." << endl;
+    cout << "Total occurrences in file contents: " << to_string(total_matches) << endl;
     return 0;
 }
